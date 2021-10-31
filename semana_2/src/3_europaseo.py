b13e375f3b29105d2d2831338ff0ec0aa09e4768
Sebas R <sebasrg0505@gmail.com>
2021-10-30 20:01:30 -0500

    ex 3

--- semana_2/src/3_europaseo.py
+++ semana_2/src/3_europaseo.py
@@ -1,9 +1,21 @@
-from operator import truediv
+from itertools import permutations
 
 
-def tsp(dists: list[list[int]]) -> int:
-    cry = True
-    pass
+def tsp(dists: list[list[int]]):
+    C = len(dists)
+    perms = permutations([x for x in range(C)])
+    best = 10000
+    for p in perms:
+        total = 0
+        for i in range(C - 1):
+            if (dists[p[i]][p[i + 1]] != None):
+                total += dists[p[i]][p[i + 1]]
+            else:
+                total = 10000
+                break
+        if total < best:
+            best = total
+    return round(best / 10) if best < 10000 else "imposible"
 
 
 P = int(input())
@@ -13,5 +25,4 @@ for casos in range(P):
     d_matrix = [[0] * C] * C
     for c in range(C):
         d_matrix[c] = [int(t) if t != "n.a" else None for t in input().split()]
-    r = tsp(d_matrix)
-    print(r)
+    print(tsp(d_matrix))
