5333f8e6678ede27b467be3891bc8e7b3f7ed028
Sebastian Rendon <sebasrg0505@gmail.com>
2022-02-02 23:37:43 -0500

    changed tags

--- modulo_13/src/4_combo_de_panas.py
+++ modulo_13/src/4_combo_de_panas.py
@@ -9,10 +9,10 @@ def dfs1(u):
     global t
 
     node = graph[u]
-    node['explored2'] = True
-    for v in node['adjacent2']:
+    node['v2'] = True
+    for v in node['adj2']:
         new_node = graph[v]
-        if not new_node['explored2']:
+        if not new_node['v2']:
             dfs1(v)
 
     stack[t] = u
@@ -23,10 +23,10 @@ def dfs2(u):
     global graph
 
     node = graph[u]
-    node['explored'] = True
-    for v in node['adjacent']:
+    node['v'] = True
+    for v in node['adj']:
         new_node = graph[v]
-        if not new_node['explored']:
+        if not new_node['v']:
             dfs2(v)
 
 
@@ -41,14 +41,13 @@ def kosaraju(g):
 
     for u in graph:
         node = graph[u]
-        if not node['explored2']:
+        if not node['v2']:
             dfs1(u)
 
     scc = 0
-    stack.reverse()
 
-    for u in stack:
-        if not graph[u]['explored']:
+    for u in reversed(stack):
+        if not graph[u]['v']:
             dfs2(u)
             scc += 1
     return scc
@@ -63,21 +62,21 @@ while C:
 
         if g.get(u) == None:
             g[u] = {
-                "explored": False,
-                "adjacent": set(),
-                "explored2": False,
-                "adjacent2": set()
+                "v": False,
+                "adj": set(),
+                "v2": False,
+                "adj2": set()
             }
         if g.get(v) == None:
             g[v] = {
-                'explored': False,
-                'adjacent': set(),
-                'explored2': False,
-                'adjacent2': set()
+                'v': False,
+                'adj': set(),
+                'v2': False,
+                'adj2': set()
             }
 
-        g[u]['adjacent'].add(v)
-        g[v]['adjacent2'].add(u)
+        g[u]['adj'].add(v)
+        g[v]['adj2'].add(u)
 
     results.append(kosaraju(g))
     C = int(input())
